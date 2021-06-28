d3d9d3cf3952f1308245afd3b1773999395645df
filipe <filipe11402@outlook.pt>
2021-06-28 20:13:52 +0100

    Added Extension method as Filter

--- DI_NET_CORE/DI_NET_CORE/Controllers/ProductController.cs
+++ DI_NET_CORE/DI_NET_CORE/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using Products.Domain.Models.Abstract;
 using Products.Domain.Models;
 using Products.API.Queries;
 using Products.API.ViewModels;
+using Products.API.ExtensionMethods;
 
 namespace Products.APIControllers
 {
@@ -14,24 +15,22 @@ namespace Products.APIControllers
         {
             var availableProducts = new ProductViewModel();
 
-<<<<<<< HEAD
-            Filter filterProduct = new Filter(20); //Valor de ID que vai filtrar
 
-            var product = new Product();
+            //Filter filterProduct = new Filter(20); //Valor de ID que vai filtrar
 
-            List<IProducts> prodList = new List<IProducts>() 
-=======
-            var availableData = new List<IProduct>
->>>>>>> 3aaba1005b61a314c293d55e4a95cdabf105ff23
+            //var product = new Product();
+
+            //List<IProducts> prodList = new List<IProducts>();
+
+            var availableData = new List<IProduct>()
             {
                 new Product(1, "Shampoo"),
                 new Product(2, "No more tears shampoo"),
                 new Product(3, "Condoms"),
             };
 
-            var query = new ProductQuery(availableData);
 
-            availableProducts.Products = query.Filter();
+            availableProducts.Products = availableData.Filter();
 
             return View(availableProducts);
         }
---
+++ DI_NET_CORE/DI_NET_CORE/ExtensionMethods/ProductExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Products.API.Queries.Abstract;
+using Products.Domain.Models.Abstract;
+
+
+namespace Products.API.ExtensionMethods
+{
+    public static class ProductExtensions
+    {
+
+        public static List<IProduct> Filter(this List<IProduct> myProducts) 
+        {
+            //Filtering myProducts when Id less than 2
+            foreach (var Product in myProducts.ToList())
+            {
+                if (Product.Id < 2) 
+                {
+                    myProducts.Remove(Product);
+                }
+            }
+
+            return myProducts;
+        }
+
+    }
+}
